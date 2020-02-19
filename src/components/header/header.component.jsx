e72de4c3b0727ca29e95147574658481f06fae44
TmothyMartel <t.m.martel@gmail.com>
2020-02-18 20:24:29 -0500

    implemented user reducer / action to app component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,7 +7,7 @@ import { auth } from '../../firebase/firebase.utils'
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 
-const Header = ({ currentUser}) => (
+const Header = ({ currentUser }) => (
     <div className='header'>
         <Link className="logo-container" to="/">
             <Logo className="logo" />
